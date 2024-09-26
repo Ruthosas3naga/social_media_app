cff95ad1c9df01138f64b105238abc9cb4342e3e
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-26 18:07:38 +0100

    django project on social media

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),    # Admin URL
     path('api/accounts/', include('accounts.urls')),  # API paths for accounts
     path('api/posts/', include('posts.urls')),
-    path('api/notifications/', include('notifications.urls')),
+    
     # API paths for posts
 ]
 
